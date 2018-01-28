7133490f1db98b6e0faf7cfd9b8acbf5f78873bb
Soumyadip <sharmasoumyadip@gmail.com>
2018-01-28 13:32:29 +0530

    profile cookie added

--- microservices/zomatonode/app/src/routes/profile-routes.js
+++ microservices/zomatonode/app/src/routes/profile-routes.js
@@ -5,11 +5,15 @@ var router = express.Router();
 var app = express();
 
 router.route('/').get(function (req, res) {
+    var cookiejar = request.jar();
+    cookiejar.getCookies('bodybuilder89', 'https://api.mydomain.com');
     var reqOpt = {
         url: 'https://auth.bodybuilder89.hasura-app.io/v1/user/info',
+        jar: cookiejar,
         method: 'GET',
         "headers": {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": 'Bearer ' + req.cookies.bodybuilder89
         }
     }
     request(reqOpt)
